7bf353165038dd4da6d1004595faf255592e8c87
veigastom <veigasbros@gmail>
2019-04-23 12:38:35 +0530

    build 02

---
+++ precache-manifest.61d0fa7d11b12c2d3b26c1fdea9b758d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "73224f5b3f626fd906b0",
+    "url": "/static/css/main.1fc5d5ef.chunk.css"
+  },
+  {
+    "revision": "d79fd66c652b5a9cb25d",
+    "url": "/static/js/1.d79fd66c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "25c99b48d1b72a76613078dea57a56b9",
+    "url": "/static/media/marc1.25c99b48.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "f6695698e1309b57a6a154bafd120976",
+    "url": "/static/media/camp1.f6695698.jpg"
+  },
+  {
+    "revision": "8515eabd254deb983692bc3272bb7eec",
+    "url": "/static/media/prashanth.8515eabd.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "73224f5b3f626fd906b0",
+    "url": "/static/js/main.73224f5b.chunk.js"
+  },
+  {
+    "revision": "0c2e4ff24debb0600886228e5dc8005c",
+    "url": "/static/media/frame.0c2e4ff2.png"
+  },
+  {
+    "revision": "06482acc35358629d532add2e38c8783",
+    "url": "/static/media/visiontag.06482acc.svg"
+  },
+  {
+    "revision": "2e0dfb3e6a80a2136da28da8eaae7bee",
+    "url": "/static/media/JohnnyLever.2e0dfb3e.jpg"
+  },
+  {
+    "revision": "2a34fc0e13f95631961547672d9a13a3",
+    "url": "/static/media/Logo.2a34fc0e.png"
+  },
+  {
+    "revision": "e5fefbfcbe37f53eda8d1e38933737ad",
+    "url": "/static/media/vision.e5fefbfc.svg"
+  },
+  {
+    "revision": "e59f2ee69b1ff7721723fe57598798da",
+    "url": "/static/media/worship.e59f2ee6.jpeg"
+  },
+  {
+    "revision": "c0d8886148bbedb165b58f7801a433e8",
+    "url": "/static/media/frame.c0d88861.svg"
+  },
+  {
+    "revision": "365351c6dd5e65794726db5c1dfd9420",
+    "url": "/static/media/vision.365351c6.png"
+  },
+  {
+    "revision": "8354902261665a004c4f56de7b03c9a9",
+    "url": "/index.html"
+  }
+];
